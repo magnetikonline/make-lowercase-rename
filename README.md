940772ea6a8aa5b82afeeae54706fee1276af668
Peter Mescalchin <peter@magnetikonline.com>
2013-02-10 22:04:27 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Make lower case filename rename
+A quick, dirty and *hacky* PHP script (with a terrible project name).
+
+Given a source directory, spits out to the console a bash script to recursively rename upper case filenames to lower cased versions - basically a bunch of `mv sourcefile sourcefilelowercase` commands.
+
+Something I needed to lower case around 30,000 digital camera images on a NAS drive which has a rather limited toolchain (BusyBox).
+
+I'm sure I could have still done this directly on the NAS using bash, but it was easier/quicker to:
+- Cobble this together
+- Run script over NFS share to the image files
+- SCP over to NAS the output as a bash script
+- Run that bash script directly on the NAS from BusyBox shell
+
+You can probably gather, this will be little to *zero* use for anyone else.
+
+## Usage
+`./makelowercasefilenamerename.php [directory-to-files] > outbash.sh`
---
+++ makelowercasefilenamerename.php
@@ -0,0 +1,95 @@
+#!/usr/bin/env php
+<?php
+// makelowercasefilenamerename.php
+
+
+
+class MakeLowerCaseFilenameRename {
+
+	const LE = "\n";
+
+	private $sourceDir = '';
+	private $writtenHeader = false;
+
+
+
+	public function __construct(array $argv) {
+
+		// validate source path
+		if (!isset($argv[1])) {
+			$this->writeLine('Please specify source directory',true);
+			return;
+		}
+
+		$this->sourceDir = rtrim($argv[1],'/');
+		if (!is_dir($this->sourceDir)) {
+			$this->writeLine('Source directory \'' . $this->sourceDir . '\' not found or invalid',true);
+			return;
+		}
+
+		$this->workDir();
+	}
+
+	private function workDir($childDir = '') {
+
+		$dirHandle = @opendir($this->sourceDir . $childDir);
+		if ($dirHandle === false) return;
+
+		while (($fileItem = readdir($dirHandle)) !== false) {
+			// skip current/parent directories
+			if (($fileItem == '.') || ($fileItem == '..')) continue;
+
+			$fileItemPath = $this->sourceDir . $childDir . '/' . $fileItem;
+
+			if (is_dir($fileItemPath)) {
+				// file is a directory, call $this->workDir() recursively
+				$this->workDir($childDir . '/' . $fileItem);
+				continue;
+			}
+
+			// output rename bash command
+			$this->emitRenameCommand($fileItemPath);
+		}
+
+		// close directory handle
+		closedir($dirHandle);
+		return;
+	}
+
+	private function emitRenameCommand($fileItemPath) {
+
+		// extract filename component and check if can be lowercased, otherwise exit
+		$filename = basename($fileItemPath);
+		$filenameLower = strtolower($filename);
+		if ($filename == $filenameLower) return;
+
+		// has bash header been written?
+		if (!$this->writtenHeader) {
+			$this->writeLine('#!/bin/bash');
+			$this->writeLine('SOURCEDIR="' . $this->escapeFilePath($this->sourceDir) . '"');
+			$this->writtenHeader = true;
+		}
+
+		// build move command
+		$sourceDirLen = strlen($this->sourceDir);
+		$this->writeLine(sprintf(
+			'mv "$SOURCEDIR%s" "$SOURCEDIR%s/%s"',
+			$this->escapeFilePath(substr($fileItemPath,$sourceDirLen)),
+			$this->escapeFilePath(substr(dirname($fileItemPath),$sourceDirLen)),
+			$this->escapeFilePath($filenameLower)
+		));
+	}
+
+	private function escapeFilePath($path) {
+
+		return str_replace('"','\"',$path);
+	}
+
+	private function writeLine($text = '',$isError = false) {
+
+		echo((($isError) ? 'Error: ' : '') . $text . self::LE);
+	}
+}
+
+
+new MakeLowerCaseFilenameRename($argv);
